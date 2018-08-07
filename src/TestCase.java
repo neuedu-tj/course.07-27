78de17b7f0a4cbfc3a5ff88da80000019c4c474b
neuedu-tj <cofcoltd@qq.com>
2018-08-07 11:31:08 +0800

    变量 / 基本数据类型

---
+++ src/TestCase.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class TestCase {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+    }
+
+}
---
+++ src/Variable_01.java
@@ -0,0 +1,15 @@
+public class Variable_01 {
+
+
+    public static void main(String[] args) {
+
+
+       float a = 128.1f;
+       int b = (int)a;
+
+
+        System.out.println(b);
+
+    }
+
+}
