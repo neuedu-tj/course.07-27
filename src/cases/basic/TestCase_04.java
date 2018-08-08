2246365b4d42be792e9f1e523b2ac10a44ce6eed
neuedu-tj <cofcoltd@qq.com>
2018-08-08 16:12:36 +0800

    add test

--- src/cases/basic/TestCase_04.java
+++ src/cases/basic/TestCase_04.java
@@ -6,30 +6,30 @@ public class TestCase_04 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
 
         int size = 10;
 
-        int i = 0;
-        while(i <= size) {
-            if(i ==0 || i==size) {
-                for (int j = 0; j < size; j++) {
-                    System.out.print("* ");
+        int i = 1;
+
+        while( i <= size ) {
+
+            if( i ==1 || i == size ) {
+                for (int j = 2; j <= size ; j++) {
+                    System.out.print("O");
                 }
             } else {
-                for (int j = 0; j <= size; j++) {
-                    if(j==0 || j==size) {
-                        System.out.print("* ");
+                for (int j = 2; j <= size ; j++) {
+                    if( j==2 || j == size ) {
+                        System.out.print("O");
                     } else {
-                        System.out.print("  ");
+                        System.out.print("M");
                     }
                 }
             }
-            System.out.println();
-
             i++;
         }
-
     }
 
 }
--- src/cases/basic/TestCase_06.java
+++ src/cases/basic/TestCase_06.java
@@ -2,19 +2,20 @@ package cases.basic;
 
 public class TestCase_06 {
 
+
+    //100$    100 只
+    //公鸡 : 5  母鸡 : 3  小鸡 1/3
     public static void main(String[] args) {
 
-        for(int x = 0; x <=100;x++) {
-           for(int y = 0 ; y<=100 ; y++) {
-               for( double z = 0 ; z<=100 ; z++) {
-                   if( (x+y+z) ==100 && (x*5 + 3*y + z/3)==100) {
-                       System.out.println(x +"," + y + "," + z);
+        for( int x = 0 ; x <= 100 ; x++) {
+           for( int y = 0 ; y<=100 ; y++) {
+               for(double z = 0 ; z <= 100 ; z++) {
+                   if( (x*5 + y*3 + z/3) ==100 && (x + y + z) == 100 ) {
+                       System.out.println( x + " , " + y + " , " + z  );
                    }
                }
            }
         }
-
-
     }
 
 }
